22ff3496fe3ca713b65ea6ffb364b92fd47441e6
vengamamba-sneha <sneha170519@gmail.com>
2025-02-03 10:25:02 +0530

    17.py

--- 17.py
+++ 17.py
@@ -1,34 +1,65 @@
-from abc import ABC, abstractmethod
-class IDatabaseOperations(ABC):
+from abc import ABC,abstractmethod
+class IDatabaseoperations(ABC):
+    def __init__(self):
+        self.datalist=[]
     @abstractmethod
-    def insert(self, data):
+    def insert(self,data):
         pass
     @abstractmethod
-    def update(self, data):
+    def update(self,data):
         pass
     @abstractmethod
-    def delete(self, data):
+    def delete(self,data):
         pass
+class SQLDatabase(IDatabaseoperations):
+    def __init__(self):
+        super().__init__()
+    def insert(self,data):
+        self.datalist.append(data)
+        print(f'inserting data {data}')
+        print(self.datalist)
+    def update(self,data):
+        self.datalist.append(data)
+        print(f'updating data {data}')
+        print(self.datalist)
+    def delete(self,data):
+        
+        if data in self.datalist:
+            self.datalist.remove(data)
+            print(f'deleting data {data}')
+            
+        else:
+            print('not found')
+        print(self.datalist)
 
-class SQLDatabase(IDatabaseOperations):
-    def insert(self, data):
-        print(f"SQL-Inserting {data}")
-    def update(self, data):
-        print(f"SQL-Updating {data}")
-    def delete(self, data):
-        print(f"SQL-Deleting {data}")
+class NoSQLDatabase(IDatabaseoperations):
 
-class NoSQLDatabase(IDatabaseOperations):
-    def insert(self, data):
-        print(f"NoSQL-Adding document {data}")
-    def update(self, data):
-        print(f"NoSQL-Modifying document {data}")
-    def delete(self, data):
-        print(f"NoSQL-Removing document {data}")
+    def __init__(self):
+        super().__init__()
+    def insert(self,data):
+        self.datalist.append(data)
+        print(f'inserting data {data}')
+        print(self.datalist)
+    def update(self,data):
+        self.datalist.append(data)
+        print(f'updating data {data}')
+        print(self.datalist)
+    def delete(self,data):
+        
+        if data in self.datalist:
+            self.datalist.remove(data)
+            print(f'deleting data {data}')
+            
+        else:
+            print('not found')
+        print(self.datalist)
 
-sql = SQLDatabase()
-nosql = NoSQLDatabase()
+s=SQLDatabase()
+s.insert('ex1')
+s.update('ex2')
+s.delete('ex1')
 
-sql.insert("employee1")
-nosql.update("employee1")
-sql.delete("employee1")
+n=NoSQLDatabase()
+n.insert('ex1')
+n.update('ex2')
+n.delete('ex1')

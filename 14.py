bcdd496ef067dd8af4345f488a44045a81c5f430
vengamamba-sneha <sneha170519@gmail.com>
2025-02-03 10:28:58 +0530

    14.py

--- 14.py
+++ 14.py
@@ -1,25 +1,15 @@
 class Notification:
-    def __init__(self,msg):
-        self.msg=msg
     def send(self):
-        print(f"{self.msg} is notified")
-class EmailNotif(Notification):
-    def __init__(self, msg):
-        super().__init__(msg)
+        print('notifications')
+class EmailNotification:
     def send(self):
-        print(f"{self.msg} is notified from mail")
-class SMSNotif(Notification):
-     def __init__(self, msg):
-        super().__init__(msg)
-     def send(self):
-        print(f"{self.msg} is notified from sms")
-def myfunc(obj):
-    obj.send()
-n=Notification("hello")
-e=EmailNotif("yo")
-sms=SMSNotif("hellooo")
-myfunc(n)
-myfunc(e)
-myfunc(sms)
-    
-    
+        print('u got an email')
+class SMSnotification:
+    def send(self):
+        print('you got a SMS')
+def fun(a):
+    a.send()
+e=EmailNotification()
+fun(e)
+s=SMSnotification()
+fun(s)

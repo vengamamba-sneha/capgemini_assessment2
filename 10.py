517cb92af28b9c5da493d85c2ff3dfc6003aeb3f
vengamamba-sneha <sneha170519@gmail.com>
2025-02-03 10:30:38 +0530

    10.py

--- 10.py
+++ 10.py
@@ -3,21 +3,20 @@ class Electronics:
         self.brand=brand
         self.price=price
     def display(self):
-        print(f"brand:{self.brand},price:{self.price}")
-class MobileDevice(Electronics):
+        print(self.brand,self.price)
+class MobilePhone(Electronics):
     def __init__(self,brand,price,battery):
         super().__init__(brand,price)
         self.battery=battery
     def display(self):
-        super().display()
-        print(f"battery:{self.battery}")
-class SmartPhone(MobileDevice):
+        print(self.battery)
+class SmartPhone(MobilePhone):
     def __init__(self, brand, price, battery,cam):
         super().__init__(brand, price, battery)
         self.cam=cam
     def display(self):
-        super().display()
-        print(f"cam:{self.cam}")
-sm=SmartPhone("pixel",100000,2000,64)
-sm.display()
+        print(self.brand,self.price,self.battery,self.cam)
+    
+s=SmartPhone('iphone',88888,90,64)
+s.display()
         
